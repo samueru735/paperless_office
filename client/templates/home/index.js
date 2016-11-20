56449e91fc05ab3e6e45eb9760214394c6410d56
samueru735 <samcontrol@hotmail.com>
2016-11-20 22:00:33 +0100

    delete docs

--- client/templates/home/index.js
+++ client/templates/home/index.js
@@ -14,12 +14,10 @@ Template.docShow.events({
   'click #btnEdit': function(){
     console.log("editing");
   },
-  'click #btnRemove': function(){
-  /*  let dataKey = $(e.target).attr("data-key"),
-    recordId = $(e.target).closest(".record-container").attr("data-record-id");
-    console.log("dataKey: " + image._id);
-    console.log("recordId: " + recordId);
-    console.log("removing"); */
+  'click #btnDeleteDoc': function(e){
+    e.preventDefault();
+    
+    $('#deleteDocModal').modal('show');
   },
   "edited .editable"(e, instance, newValue){
     // pick up the field that was edited
@@ -41,7 +39,15 @@ Template.docShow.events({
       newLabels[recordId] = newValue;
      Images.update({'_id':dataKey}, {$set:{"meta":{"labels":newLabels }}});      
      //image.update({'labels': labelToChange }, {$set:{"meta":{"labels.$":newValue }}});      
-    }
+
+    }    
+  }
+})
+
+Template.deleteDocModal.events({
+  'click #btnConfirmDelete': function(e){
+    //e.preventDefault();
+    $('#deleteDocModal').modal('hide');      
   }
 })
 
@@ -59,12 +65,12 @@ Template.labelSelector.events({
 Template.uploadedFiles.helpers({
 
   uploadedFiles: function () {        
-    return Images.find();
+    return Images.find({}, {sort: { 'meta.date': -1}}) ;
   }
 });
 Template.file.helpers({
   imageFiles: function () {
-    return Images.find();
+    return Images.find({}, {sort: { 'meta.date': -1}});
   }
 });
 
